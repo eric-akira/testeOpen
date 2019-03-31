94039d71d891c1620a7520d7553735ae190f847c
Eric Akira <Eric Akira>
2019-03-30 23:25:00 -0300

    Alterando pagina tabs e importando novas paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HistoryPage } from '../pages/history/history';
+import { SearchPage } from '../pages/search/search';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    SearchPage,
+    HistoryPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +33,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    SearchPage,
+    HistoryPage
   ],
   providers: [
     StatusBar,
